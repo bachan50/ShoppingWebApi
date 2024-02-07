6f05f51f9be5320001a168f276f6f0d4263975b5
bachan50 <shiv.bachan50@gmail.com>
2024-02-07 12:22:45 +0530

    Initailize DB Connection

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingWebApi.EfCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<EF_DataContext>(o=>o.UseNpgsql(builder.Configuration.GetConnectionString("Ef_Postgres_Db")));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
